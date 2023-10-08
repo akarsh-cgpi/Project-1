ffb5da7b0b325a8889235179aa8a9267a3c091ab
shivanighogare <147234955+shivanighogare@users.noreply.github.com>
2023-10-08 09:12:56 +0000

    new change

--- stationary/src/App.js
+++ stationary/src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>deepak bhau</h1>
+        <h1>shivani</h1>
         <p>
-         hey there we started a project
+         hey there we started a stationary project
         </p>
         <a
           className="App-link"
