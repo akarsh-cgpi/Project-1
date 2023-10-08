002ab768e5f353fc9762a7a82d547e277a6b95dd
shindebharati <147237789+shindebharati@users.noreply.github.com>
2023-10-08 09:12:58 +0000

    new change

--- stationary/src/App.js
+++ stationary/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <h1> this is Stationary</h1>
         <p>
          hey there we started
         </p>
